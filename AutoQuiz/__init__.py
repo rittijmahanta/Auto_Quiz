87ac3163a0de302724ba5c344ee06c6df9b22e91
rittijmahanta <rittijmahanta@gmail.com>
2025-05-02 22:55:14 +0530

    Add files via upload

---
+++ AutoQuiz/__init__.py
@@ -0,0 +1,5 @@
+# Constructor for questgen
+from __future__ import absolute_import
+from AutoQuiz.encoding import encoding
+from AutoQuiz.mcq import mcq
+from AutoQuiz.main import QGen, BoolQGen, AnswerPredictor
